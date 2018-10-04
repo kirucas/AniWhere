eb2a8fae672e1a1c87bc66aa6124df4d0535073f
choi <wmffkdla@naver.com>
2018-10-04 18:50:37 +0900

    miss update 10-04 06:50

--- src/main/java/com/animal/aniwhere/web/board/animal/bird/BirdTipController.java
+++ src/main/java/com/animal/aniwhere/web/board/animal/bird/BirdTipController.java
@@ -1,10 +1,15 @@
 package com.animal.aniwhere.web.board.animal.bird;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BirdTipController {
 
+	@RequestMapping("/animal/bird/tip.aw")
+	public String bird_tip_list() throws Exception {
+		return "board/animal/bird/tip/tip_list.tiles";
+	}////////// free_main
 	
 	
 }//////////////////// TipController class
