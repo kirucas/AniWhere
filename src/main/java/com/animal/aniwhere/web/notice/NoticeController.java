63d4908cc41e4c5d70e8979686a459fec6654beb
kirucas <kolp00103@gmail.com>
2018-10-04 20:07:12 +0900

    notice folder & controller 작성
    view 폴더 바로 하위에 notice folder 생성 및 임시 페이지 작성
    java 쪽 web 바로 하위에 notice 생성 및 해당 컨트롤러 생성 및 이동 메소드 작성

---
+++ src/main/java/com/animal/aniwhere/web/notice/NoticeController.java
@@ -0,0 +1,14 @@
+package com.animal.aniwhere.web.notice;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class NoticeController {
+
+	@RequestMapping("/notice.aw")
+	public String notice() throws Exception {
+		return "/notice/notice.tiles";
+	}
+	
+}
