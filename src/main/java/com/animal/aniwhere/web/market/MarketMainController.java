b1ab84cbc720510b731abad54ee6d5d2ca46ccfb
kimilho <drain20@naver.com>
2018-10-24 19:30:50 +0900

    거래게시판 쿠르드 작업중...
    중간세이브

--- src/main/java/com/animal/aniwhere/web/market/MarketMainController.java
+++ src/main/java/com/animal/aniwhere/web/market/MarketMainController.java
@@ -12,11 +12,12 @@ public class MarketMainController {
 		return "market/MainMaket.tiles";
 	}////////// market_main
 	
+	/*
 	@RequestMapping("/market/buyinside.aw")
 	public String buyinside() throws Exception {
 		return "market/inside/buyinside.tiles";
 		
-		
+		*/
 	}
 	@RequestMapping("/market/sellinside.aw")
 	public String sellinside() throws Exception {
@@ -33,13 +34,13 @@ public class MarketMainController {
 		
 		
 	}
-	
+	/*
 	@RequestMapping("/market/buyWrite.aw")
 	public String buyWrite() throws Exception {
 		return "market/write/buyWrite.tiles";
 		
 		
-	}
+	}*/
 	
 	@RequestMapping("/market/SellWrite.aw")
 	public String SellWrite() throws Exception {
