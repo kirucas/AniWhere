0662f63529de6e31541f0581688e95df2a6d2552
kirucas <kolp00103@gmail.com>
2018-10-25 21:57:49 +0900

    ERD 수정 / Animal.xml query 수정

--- src/main/SQL/database.sql
+++ src/main/SQL/database.sql
@@ -2,6 +2,7 @@
 /* Drop Tables */
 
 DROP TABLE notice CASCADE CONSTRAINTS;
+DROP TABLE qna CASCADE CONSTRAINTS;
 DROP TABLE admin_member CASCADE CONSTRAINTS;
 DROP TABLE drafting CASCADE CONSTRAINTS;
 DROP TABLE mating CASCADE CONSTRAINTS;
@@ -23,7 +24,7 @@ DROP TABLE freeboard CASCADE CONSTRAINTS;
 DROP TABLE lost_animal CASCADE CONSTRAINTS;
 DROP TABLE movie_cmt CASCADE CONSTRAINTS;
 DROP TABLE movie CASCADE CONSTRAINTS;
-DROP TABLE photo_cmd CASCADE CONSTRAINTS;
+DROP TABLE photo_cmt CASCADE CONSTRAINTS;
 DROP TABLE photo_link CASCADE CONSTRAINTS;
 DROP TABLE photo CASCADE CONSTRAINTS;
 DROP TABLE quest_cmt CASCADE CONSTRAINTS;
@@ -54,7 +55,7 @@ CREATE TABLE admin_member
 CREATE TABLE animal
 (
 	ani_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	ani_name nvarchar2(20) NOT NULL,
 	ani_age number NOT NULL,
 	ani_gender varchar2(1) NOT NULL CHECK (ani_gender IN ('F', 'M', 'U')),
@@ -87,7 +88,7 @@ CREATE TABLE drafting
 CREATE TABLE freeboard
 (
 	free_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	free_title nvarchar2(50) NOT NULL,
 	free_content nvarchar2(2000) NOT NULL,
 	free_hit number DEFAULT 0,
@@ -102,7 +103,7 @@ CREATE TABLE freeboard_cmt
 (
 	cmt_no number NOT NULL,
 	origin_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	cmt_content nvarchar2(200) NOT NULL,
 	hit number,
 	regidate date DEFAULT sysdate,
@@ -114,7 +115,7 @@ CREATE TABLE group_buy
 (
 	buy_no number NOT NULL,
 	no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	buy_count number NOT NULL,
 	PRIMARY KEY (buy_no)
 );
@@ -141,7 +142,7 @@ CREATE TABLE lost_animal
 CREATE TABLE market_buy
 (
 	no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	animal_code number NOT NULL,
 	title nvarchar2(50) NOT NULL,
 	content nvarchar2(2000) NOT NULL,
@@ -155,7 +156,7 @@ CREATE TABLE market_buy_cmt
 (
 	cmt_no number NOT NULL,
 	origin_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	cmt_content nvarchar2(200) NOT NULL,
 	hit number DEFAULT 0,
 	regidate date DEFAULT sysdate,
@@ -166,7 +167,7 @@ CREATE TABLE market_buy_cmt
 CREATE TABLE market_group_buy
 (
 	no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	animal_code number NOT NULL,
 	title nvarchar2(50) NOT NULL,
 	content nvarchar2(2000) NOT NULL,
@@ -182,7 +183,7 @@ CREATE TABLE market_group_buy_cmt
 (
 	cmt_no number NOT NULL,
 	origin_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	cmt_content nvarchar2(200) NOT NULL,
 	hit number DEFAULT 0,
 	regidate date DEFAULT sysdate,
@@ -193,7 +194,7 @@ CREATE TABLE market_group_buy_cmt
 CREATE TABLE market_sell
 (
 	no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	animal_code number NOT NULL,
 	title nvarchar2(50) NOT NULL,
 	content nvarchar2(2000) NOT NULL,
@@ -207,7 +208,7 @@ CREATE TABLE market_sell_cmt
 (
 	cmt_no number NOT NULL,
 	origin_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	cmt_content nvarchar2(200) NOT NULL,
 	hit number DEFAULT 0,
 	regidate date DEFAULT sysdate,
@@ -218,7 +219,7 @@ CREATE TABLE market_sell_cmt
 CREATE TABLE mating
 (
 	mating_no number NOT NULL,
-	ani_no number NOT NULL,
+	ani_no number,
 	mating_loc number NOT NULL,
 	mating_regidate date DEFAULT SYSDATE,
 	PRIMARY KEY (mating_no)
@@ -244,7 +245,7 @@ CREATE TABLE member
 CREATE TABLE miss_find
 (
 	no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	animal_code number NOT NULL,
 	title nvarchar2(50) NOT NULL,
 	content nvarchar2(2000) NOT NULL,
@@ -258,7 +259,7 @@ CREATE TABLE miss_find_cmt
 (
 	cmt_no number NOT NULL,
 	origin_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	cmt_content nvarchar2(200) NOT NULL,
 	hit number DEFAULT 0,
 	regidate date DEFAULT sysdate,
@@ -269,7 +270,7 @@ CREATE TABLE miss_find_cmt
 CREATE TABLE miss_see
 (
 	no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	animal_code number NOT NULL,
 	title nvarchar2(50) NOT NULL,
 	content nvarchar2(2000) NOT NULL,
@@ -283,7 +284,7 @@ CREATE TABLE miss_see_cmt
 (
 	cmt_no number NOT NULL,
 	origin_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	cmt_content nvarchar2(200) NOT NULL,
 	hit number DEFAULT 0,
 	regidate date DEFAULT sysdate,
@@ -294,7 +295,7 @@ CREATE TABLE miss_see_cmt
 CREATE TABLE movie
 (
 	movie_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	movie_title nvarchar2(50) NOT NULL,
 	movie_content nvarchar2(2000) NOT NULL,
 	movie_count number DEFAULT 0,
@@ -309,7 +310,7 @@ CREATE TABLE movie_cmt
 (
 	cmt_no number NOT NULL,
 	origin_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	cmt_content nvarchar2(200) NOT NULL,
 	hit number DEFAULT 0,
 	regidate date DEFAULT SYSDATE,
@@ -320,7 +321,7 @@ CREATE TABLE movie_cmt
 CREATE TABLE notice
 (
 	no number NOT NULL,
-	am_no number NOT NULL,
+	am_no number,
 	title nvarchar2(50) NOT NULL,
 	content nvarchar2(2000) NOT NULL,
 	regidate date DEFAULT SYSDATE,
@@ -332,7 +333,7 @@ CREATE TABLE notice
 CREATE TABLE photo
 (
 	photo_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	photo_title nvarchar2(50) NOT NULL,
 	photo_content nvarchar2(2000) NOT NULL,
 	photo_count number DEFAULT 0,
@@ -343,11 +344,11 @@ CREATE TABLE photo
 );
 
 
-CREATE TABLE photo_cmd
+CREATE TABLE photo_cmt
 (
 	cmt_no number NOT NULL,
 	origin_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	cmt_content nvarchar2(200) NOT NULL,
 	hit number DEFAULT 0,
 	regidate date DEFAULT SYSDATE,
@@ -364,10 +365,24 @@ CREATE TABLE photo_link
 );
 
 
+CREATE TABLE qna
+(
+	no number NOT NULL,
+	mem_no number,
+	title nvarchar2(50) NOT NULL,
+	quest_ct nvarchar2(2000) NOT NULL,
+	quest_date date DEFAULT SYSDATE,
+	am_no number,
+	answer_ct nvarchar2(2000),
+	answer_date date,
+	PRIMARY KEY (no)
+);
+
+
 CREATE TABLE quest
 (
 	quest_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	quest_title nvarchar2(50) NOT NULL,
 	quest_content nvarchar2(2000) NOT NULL,
 	quest_count number DEFAULT 0,
@@ -384,7 +399,7 @@ CREATE TABLE quest_cmt
 (
 	cmt_no number NOT NULL,
 	origin_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	cmt_content nvarchar2(200) NOT NULL,
 	hit number DEFAULT 0,
 	regidate date DEFAULT SYSDATE,
@@ -395,7 +410,7 @@ CREATE TABLE quest_cmt
 CREATE TABLE reservation
 (
 	rv_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	store_no number NOT NULL,
 	apply_date date DEFAULT sysdate,
 	booking_date date NOT NULL,
@@ -435,7 +450,7 @@ CREATE TABLE store_location
 CREATE TABLE tip
 (
 	tip_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	tip_title nvarchar2(50) NOT NULL,
 	tip_content nvarchar2(2000) NOT NULL,
 	tip_count number DEFAULT 0,
@@ -450,7 +465,7 @@ CREATE TABLE tip_cmt
 (
 	cmt_no number NOT NULL,
 	origin_no number NOT NULL,
-	mem_no number NOT NULL,
+	mem_no number,
 	cmt_content nvarchar2(200) NOT NULL,
 	hit number DEFAULT 0,
 	regidate date DEFAULT SYSDATE,
@@ -464,12 +479,21 @@ CREATE TABLE tip_cmt
 ALTER TABLE notice
 	ADD FOREIGN KEY (am_no)
 	REFERENCES admin_member (am_no)
+	ON DELETE SET NULL
+;
+
+
+ALTER TABLE qna
+	ADD FOREIGN KEY (am_no)
+	REFERENCES admin_member (am_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE mating
 	ADD FOREIGN KEY (ani_no)
 	REFERENCES animal (ani_no)
+	ON DELETE CASCADE
 ;
 
 
@@ -506,30 +530,35 @@ ALTER TABLE miss_see
 ALTER TABLE freeboard_cmt
 	ADD FOREIGN KEY (origin_no)
 	REFERENCES freeboard (free_no)
+	ON DELETE CASCADE
 ;
 
 
 ALTER TABLE market_buy_cmt
 	ADD FOREIGN KEY (origin_no)
 	REFERENCES market_buy (no)
+	ON DELETE CASCADE
 ;
 
 
 ALTER TABLE group_buy
 	ADD FOREIGN KEY (no)
 	REFERENCES market_group_buy (no)
+	ON DELETE CASCADE
 ;
 
 
 ALTER TABLE market_group_buy_cmt
 	ADD FOREIGN KEY (origin_no)
 	REFERENCES market_group_buy (no)
+	ON DELETE CASCADE
 ;
 
 
 ALTER TABLE market_sell_cmt
 	ADD FOREIGN KEY (origin_no)
 	REFERENCES market_sell (no)
+	ON DELETE CASCADE
 ;
 
 
@@ -542,193 +571,231 @@ ALTER TABLE drafting
 ALTER TABLE drafting
 	ADD FOREIGN KEY (receive_no)
 	REFERENCES mating (mating_no)
+	ON DELETE CASCADE
 ;
 
 
 ALTER TABLE animal
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE freeboard
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
-	ON DELETE SET 0
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE freeboard_cmt
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE group_buy
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE market_buy
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE market_buy_cmt
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE market_group_buy
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE market_group_buy_cmt
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE market_sell
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE market_sell_cmt
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE miss_find
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE miss_find_cmt
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE miss_see
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE miss_see_cmt
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE movie
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE movie_cmt
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE photo
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
-ALTER TABLE photo_cmd
+ALTER TABLE photo_cmt
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
+;
+
+
+ALTER TABLE qna
+	ADD FOREIGN KEY (mem_no)
+	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE quest
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE quest_cmt
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE reservation
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE tip
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE tip_cmt
 	ADD FOREIGN KEY (mem_no)
 	REFERENCES member (mem_no)
+	ON DELETE SET NULL
 ;
 
 
 ALTER TABLE miss_find_cmt
 	ADD FOREIGN KEY (origin_no)
 	REFERENCES miss_find (no)
+	ON DELETE CASCADE
 ;
 
 
 ALTER TABLE miss_see_cmt
 	ADD FOREIGN KEY (origin_no)
 	REFERENCES miss_see (no)
+	ON DELETE CASCADE
 ;
 
 
 ALTER TABLE movie_cmt
 	ADD FOREIGN KEY (origin_no)
 	REFERENCES movie (movie_no)
+	ON DELETE CASCADE
 ;
 
 
-ALTER TABLE photo_cmd
+ALTER TABLE photo_cmt
 	ADD FOREIGN KEY (origin_no)
 	REFERENCES photo (photo_no)
+	ON DELETE CASCADE
 ;
 
 
 ALTER TABLE photo_link
 	ADD FOREIGN KEY (photo_no)
 	REFERENCES photo (photo_no)
+	ON DELETE CASCADE
 ;
 
 
 ALTER TABLE quest_cmt
 	ADD FOREIGN KEY (origin_no)
 	REFERENCES quest (quest_no)
+	ON DELETE CASCADE
 ;
 
 
 ALTER TABLE reservation
 	ADD FOREIGN KEY (store_no)
 	REFERENCES store_location (no)
+	ON DELETE CASCADE
 ;
 
 
 ALTER TABLE tip_cmt
 	ADD FOREIGN KEY (origin_no)
 	REFERENCES tip (tip_no)
+	ON DELETE CASCADE
 ;
 
 
