13c3193d406d7a0227345ef32ece6fa16ef2c5a1
ygh <chieven@live.co.kr>
2018-10-26 12:19:36 +0900

    s3 추가중 실험도 못해봄

--- src/main/java/com/animal/aniwhere/web/board/animal/bird/BirdPhotoController.java
+++ src/main/java/com/animal/aniwhere/web/board/animal/bird/BirdPhotoController.java
@@ -1,7 +1,9 @@
 package com.animal.aniwhere.web.board.animal.bird;
 
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,6 +15,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.animal.aniwhere.web.board.FileUpDownUtils;
 
 @Controller
@@ -39,11 +46,42 @@ public class BirdPhotoController {
 			String newFilename = FileUpDownUtils.getNewFileName(phisicalPath, upload.getOriginalFilename());
 			System.out.println("파일 이름:"+newFilename);
 			try {
-				upload.transferTo(new File(phisicalPath+File.separator+newFilename));
+				//upload.transferTo(new File(phisicalPath+File.separator+newFilename));
+				putObject(phisicalPath+File.separator+newFilename);
+				readObjects();
 			} catch (Exception e) { e.printStackTrace();}
 		}
 		System.out.println("업로드으!");
         return "";
 	}/// writeComplete
+	
+	// AWS s3 업로드 메소드
+	public static final String BUCKET_NAME="aniwhere"; // s3 버킷(저장소)이름
+	public static final String END_POINT="http://aniwhere.s3-website.ap-northeast-2.amazonaws.com";
+	public void putObject(String file_path) {
+        String key_name = Paths.get(file_path).getFileName().toString();
+
+        System.out.format("Uploading %s to S3 bucket %s...\n", file_path, BUCKET_NAME);
+        final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+        try {
+        	// 업로드
+            s3.putObject(BUCKET_NAME, key_name, new File(file_path));
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        }
+        System.out.println("Done!");
+	}/// putObject
+	
+	// 버킷에서 파일 목록 불러오기
+	public void readObjects() {
+	    System.out.format("Objects in S3 bucket %s:\n", BUCKET_NAME);
+	    final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+	    ListObjectsV2Result result = s3.listObjectsV2(BUCKET_NAME);
+	    List<S3ObjectSummary> objects = result.getObjectSummaries();
+	    for (S3ObjectSummary os: objects) {
+	        System.out.println("* " + os.getKey());
+	    }
+	}/// readObjects
 	
 }//////////////////// PhotoController class
