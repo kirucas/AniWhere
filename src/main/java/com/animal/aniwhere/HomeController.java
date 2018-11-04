93e0fcbc66ff1d15a6265360e782e3d972b0ba45
parkyounju <park3jr@nate.com>
2018-11-04 23:32:32 +0900

    asdf

--- src/main/java/com/animal/aniwhere/HomeController.java
+++ src/main/java/com/animal/aniwhere/HomeController.java
@@ -1,7 +1,9 @@
 package com.animal.aniwhere;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -10,16 +12,24 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.AutoPopulatingList.ElementInstantiationException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.animal.aniwhere.service.NoticeDTO;
 import com.animal.aniwhere.service.impl.miss.LostAnimalServiceImpl;
 import com.animal.aniwhere.service.miss.LostAnimalDTO;
+
+import junit.framework.Test;
 
 
 /**
@@ -62,5 +72,27 @@ public class HomeController {
 		model.addAttribute("serverTime",new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
 		return "mainTemplate.tiles";
 	}
-	
+	/*
+	@RequestMapping("/main.aw")
+	public String parser() {
+		List<NoticeDTO> list = new ArrayList<NoticeDTO>();
+		Document doc;
+		try {
+			doc = Jsoup.connect("http://www.kangsamo.com/news/section_list_all.html?sec_no=61").get();
+			for(int i=0; i<10; i++) {
+				Elements elementTitle = doc.select("h2.clamp").eq(i);
+				String text = elementTitle.text();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		NoticeDTO dto_element = new NoticeDTO();
+		dto_element.getElementTitle();
+		dto_element.getElementImg();
+		dto_element.getElementContent();
+		
+
+		
+		return "mainTemplate.tiles";
+	}*/
 }
