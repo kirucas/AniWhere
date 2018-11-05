bde11c5fcc90f0dec274585f7133f69d6a347368
kirucas <kolp00103@gmail.com>
2018-11-05 11:20:31 +0900

    ERD 수정 | Drafting DAO/Service/Query 추가

--- src/main/SQL/database.sql
+++ src/main/SQL/database.sql
@@ -91,12 +91,13 @@ CREATE TABLE buy_cmt
 
 CREATE TABLE drafting
 (
-	dtf_no number NOT NULL,
+	dft_no number NOT NULL,
 	send_no number NOT NULL,
 	receive_no number NOT NULL,
 	apply number(1),
-	dtf_date date DEFAULT SYSDATE,
-	PRIMARY KEY (dtf_no)
+	dft_date date DEFAULT SYSDATE,
+	result_date date,
+	PRIMARY KEY (dft_no)
 );
 
 
@@ -585,15 +586,15 @@ ALTER TABLE sell_cmt
 
 
 ALTER TABLE drafting
-	ADD FOREIGN KEY (receive_no)
+	ADD FOREIGN KEY (send_no)
 	REFERENCES mating (mating_no)
-	ON DELETE CASCADE
 ;
 
 
 ALTER TABLE drafting
-	ADD FOREIGN KEY (send_no)
+	ADD FOREIGN KEY (receive_no)
 	REFERENCES mating (mating_no)
+	ON DELETE CASCADE
 ;
 
 
