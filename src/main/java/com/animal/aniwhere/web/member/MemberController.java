76692415b4129d0f5335d9fe0bb1ad8d2d7721c5
KwakCheol <KwakCheol0517@gmail.com>
2018-11-07 12:00:02 +0900

    FCM수정

--- src/main/java/com/animal/aniwhere/web/member/MemberController.java
+++ src/main/java/com/animal/aniwhere/web/member/MemberController.java
@@ -14,6 +14,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.codec.net.QCodec;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +45,8 @@ import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.animal.aniwhere.service.AwsS3Utils;
+import com.animal.aniwhere.service.QRCode_Generator;
+import com.animal.aniwhere.service.impl.ReservationServiceImpl;
 import com.animal.aniwhere.service.impl.member.AndroidTokenServiceImpl;
 import com.animal.aniwhere.service.impl.member.AnimalServiceImpl;
 import com.animal.aniwhere.service.impl.member.MemberServiceImpl;
@@ -54,6 +57,7 @@ import com.github.scribejava.core.model.OAuth2AccessToken;
 import com.google.android.gcm.server.Message;
 import com.google.android.gcm.server.MulticastResult;
 import com.google.android.gcm.server.Sender;
+import com.google.zxing.qrcode.encoder.QRCode;
 
 @Controller
 public class MemberController {
@@ -70,6 +74,9 @@ public class MemberController {
 
 	@Resource(name = "tokenService")
 	private AndroidTokenServiceImpl androidservice;
+	
+	@Resource(name = "reservationService")
+	private ReservationServiceImpl reservationservice;
 
 	@Autowired
 	PasswordEncoder passwordEncoder;
@@ -454,7 +461,7 @@ public class MemberController {
 
 	@ResponseBody
 	@RequestMapping(value = "/FireBasePushAsyncTask.awa", method = RequestMethod.POST, produces = "text/plain; charset=UTF-8")
-	public String androidGetToken(@RequestParam Map map) throws Exception {
+	public String FireBasePushAsyncTask(@RequestParam Map map) throws Exception {
 		Map result = androidservice.selectOne(map);
 		if (!result.get("MTK_TOKEN").equals("null")) {
 			System.out.println("FireBasePushAsyncTask");
@@ -472,6 +479,10 @@ public class MemberController {
 	        try {
 		        MulticastResult multicast = sender.send(msg,token,3);
 		        if(multicast != null) {
+		        	System.out.println("=======================1");
+		        	System.out.println(map.get("visit_time").toString());
+		        	QRCode_Generator.changeQRLink(reservationservice, map);
+		        	System.out.println("=======================2");
 		        	return "true";
 		        }
 	        }catch(Exception e) {
@@ -492,7 +503,6 @@ public class MemberController {
 		System.out.println(map.get("mem_no"));
 
 		if (!obj.equals("null")) {
-			System.out.println("널인데 왜 들어오지");
 			Map result = androidservice.selectOne(map);
 			if (result == null) {
 				androidservice.insert(map);
@@ -502,9 +512,8 @@ public class MemberController {
 				System.out.println("========2=======");
 				System.out.println(affect);
 				if (affect == 1) {
-					System.out.println("========4=======");
 					androidservice.insert(map);
-					System.out.println("========4=======");
+					System.out.println("========3=======");
 				}
 			}
 		}
